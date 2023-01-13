486f0326a0618a4d03f917c50e9c39039feffbfc
heienicklea <heienicklea@gmail.com>
2023-01-13 10:36:02 -0600

    Hello World in Python

--- main.py
+++ main.py
@@ -1,2 +1,30 @@
 
 print("HelloWorld")
+
+if 5 > 2:
+    print("Five is greater than Two!")
+
+x - 5
+
+#Code and Spartans put together
+
+y = "This is Python! *in Spartacus voice*" #fantastic new movie idea
+
+#print("I want to say hello world")
+print("Comments will stop that from happening")
+
+#I
+#Can
+#Talk
+#On
+#More
+#Than
+#One
+#Line
+
+
+
+
+
+
+
