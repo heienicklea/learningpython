9dadc26bcdd31e8462c4c63d7e3adab7b8d0b32a
heienicklea <heienicklea@gmail.com>
2023-01-13 10:48:39 -0600

    More tutorial progress

--- main.py
+++ main.py
@@ -30,6 +30,45 @@ print(x)
 print(y)
 
 
+#explicit creation of variables
+
+#I really want a string
+x = str(3) #this is a string that is '3'
+print(type(x))
+#I really want an integer
+y = int(3) #this is an integer that is 3
+print(type(y))
+#I really want a float
+z = float(3) #this is a float that is 3.0
+print(type(z))
+
+#These are the same things
+x = "John"
+print(x)
+x = 'John'
+print(x)
+
+#These are different variables (Capitalized X will not overwrite x)
+x = "John"
+print(x)
+X = "Sally"
+print(x)
+
+#legal variables names:
+
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+
+#illegal variable names:
+#  --  2myvar = "John"
+#  --  my-var = "John"
+#  --  my var = "John"
+
+
 
 
 
