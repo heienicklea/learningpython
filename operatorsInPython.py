f7910a278f913c64949be5a39ef05ee8e16dd7ee
heienicklea <heienicklea@gmail.com>
2023-01-13 13:51:20 -0600

    Operators in python file added

---
+++ operatorsInPython.py
@@ -0,0 +1,39 @@
+
+#Basic operations:
+#Addition +
+#Subtraction -
+#Multiplication *
+#Division /
+#Modulus %
+#Exponentiation x ** y
+#Floor Division //
+
+#ASsignment Operators
+# Equal =
+# Plus Equal +=
+# Subtraction Equal -=
+# Division Equal /=
+# Modulus Equal %=
+# Floor Division Equal //=
+# Exponentiation Equal **=
+# Ampersand Equal &=
+
+#Comparison Operators
+# == Equal?
+# != NotEqual?
+# >
+# <
+# >=
+# <=
+
+#Logical Operators
+# and returns true if both are true
+# or returns true if one of statements is true
+# not Returns true when false
+
+#Identity Operators
+#is -- returns true if both variable are the same object
+#is not -- returns true if both variables are not the same object
+
+# in -- returns true if a sequence wth the specified vlaue is present in the object
+# not in -- returns true if a sequence with the specified value is NOT in the object
