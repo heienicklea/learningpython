a1b5e0b073302d5e84147825a0ed6aa019f76948
heienicklea <heienicklea@gmail.com>
2023-01-13 15:03:37 -0600

    List in Python Tutorial work

--- listsInPython.py
+++ listsInPython.py
@@ -182,6 +182,46 @@ thislist = ["apple", "banana", "cherry"]
 mylist = thislist.copy()
 print(mylist)
 
+#OR
 
+thislist = ["apple", "banana", "cherry"]
+mylist = list(thislist)
+print(mylist)
 
+#COMBINING LISTS TOGETHER
 
+list1 = ["a", "b", "c"]
+list2 = [1, 2, 3]
+
+list3 = list1 + list2
+print(list3) #prints ['a', 'b', 'c', 1, 2, 3]
+
+#Appending with a for loop
+list1 = ["a", "b" , "c"]
+list2 = [1, 2, 3]
+
+for x in list2:
+  list1.append(x) #appends to end of list one by one
+
+print(list1)
+
+#Extend method to add all to the end of the list
+
+list1 = ["a", "b" , "c"]
+list2 = [1, 2, 3]
+
+list1.extend(list2)
+print(list1)
+
+#METHODS:
+#append()	Adds an element at the end of the list
+#clear()	Removes all the elements from the list
+#copy()	Returns a copy of the list
+#count()	Returns the number of elements with the specified value
+#extend()	Add the elements of a list (or any iterable), to the end of the current list
+#index()	Returns the index of the first element with the specified value
+#insert()	Adds an element at the specified position
+#pop()	Removes the element at the specified position
+#remove()	Removes the item with the specified value
+#reverse()	Reverses the order of the list
+#sort()	Sorts the list
