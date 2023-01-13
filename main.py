ad2a92b044cd06dbddeb23c8352e50b2be08e985
heienicklea <heienicklea@gmail.com>
2023-01-13 10:39:00 -0600

    Fixed x - 5 versus x = 5 bug

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ print("HelloWorld")
 if 5 > 2:
     print("Five is greater than Two!")
 
-x - 5
+x = 5
 
-#Code and Spartans put together
+#Codeand Spartans put together
 
 y = "This is Python! *in Spartacus voice*" #fantastic new movie idea
 
@@ -21,6 +21,15 @@ print("Comments will stop that from happening")
 #Than
 #One
 #Line
+
+
+#Print numbers and strings
+x = 5
+y = "John"
+print(x)
+print(y)
+
+
 
 
 
