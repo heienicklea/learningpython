2fee2a774eba6898619d8d56dc2b1566c2bff99f
heienicklea <heienicklea@gmail.com>
2023-01-20 11:42:46 -0600

    Some more tutorials

--- lamdaInPython.py
+++ lamdaInPython.py
@@ -10,3 +10,15 @@ print(x(5, 6, 2))
 
 def myfunc(n):
   return lambda a : a * n
+
+#creating a lambda with the return statement
+mydouble = myfunc(2) #myfunc returns "lambda a : a * 2"
+
+print(mydouble(11))
+
+#Now you can create multiple uses
+mydoubler = myfunc(2)
+mytripler = myfunc(3)
+
+print(mydoubler(11))
+print(mytripler(11))
