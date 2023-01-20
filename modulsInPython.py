0822a4b6db6fa575d7a10d08a6a2387300981de0
heienicklea <heienicklea@gmail.com>
2023-01-20 17:56:12 -0600

    Some more tutorials

---
+++ modulsInPython.py
@@ -0,0 +1,16 @@
+#modulesInPython
+
+#To create a module, save the code you want in a file with the extenion.py
+
+def greeting(name):
+    print("Hello," + name)
+
+
+#Built-in Modules
+
+#dir() Function list all the function names in a moduile
+import platform
+
+x = dir(platform)
+print(x)
+
