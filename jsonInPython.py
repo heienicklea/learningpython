e795e04ec6b54dbe982bf3d8ab218473da7a4755
heienicklea <heienicklea@gmail.com>
2023-01-20 18:22:36 -0600

    Some more tutorials

---
+++ jsonInPython.py
@@ -0,0 +1,30 @@
+#JSONinPYTHON
+
+# a Python object (dict):
+
+import json
+
+#JSON TO PYTHON
+
+# some JSON:
+x =  '{ "name":"John", "age":30, "city":"New York"}'
+
+# parse x:
+y = json.loads(x)
+
+# the result is a Python dictionary:
+print(y["age"])
+
+
+#Python to JSON
+x = {
+    "name" : "John",
+    "age": 30,
+    "city": "New York"
+}
+
+# convert into JSON:
+y = json.dumps(x)
+
+# the result is a JSON string:
+print(y)
---
+++ mathInPython.py
@@ -0,0 +1,27 @@
+#mathInPython
+
+#Functions provides the minimum or maximum
+
+x = min(5, 10, 25)
+y = max(5, 10, 25)
+
+print(x)
+print(y)
+
+#Returns value of 4 to the power of 3
+y = pow(4, 3)
+
+print(y)
+
+import math
+
+z = math.sqrt(64)
+
+print(z)
+
+#Returns 
+a = math.ceil(1.4)
+b = math.floor(1.4)
+
+print(a) # returns 2
+print(b) # returns 1
