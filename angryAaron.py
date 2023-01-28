2c05e293d58b1adea68ffe73fe13c9534c52a31a
heienicklea <heienicklea@gmail.com>
2023-01-28 17:04:56 -0600

    Starting web scraping

---
+++ angryAaron.py
@@ -0,0 +1,13 @@
+
+#Aaron's therapy sessions
+#- print what you want to say one million times
+
+jakeFuckedUp = True
+i = 0
+while jakeFuckedUp:
+    print("HAHA JAKE FUCKED UP AND SAID TOO MUCH. WHAT A LOSER")
+    i += 1
+    print(i)
+    if i == 1000000:
+        break
+
